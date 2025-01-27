1e1151df8a7458d274714db4147e66265a09c758
ND <aspizhav@gmail.com>
2025-01-27 18:54:08 +0100

    init public project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Main } from './components/Main'
+import { Screens } from './components/Screens'
+import { Platform } from './components/Platfotm'
+import { Benefits } from './components/Benefits'
+import { Quote } from './components/Quote'
+import { Portfolio } from './components/Portfolio'
+import { Footer } from './components/Footer'
+
+function App() {
+	return (
+		<div>
+			<Main></Main>
+			<Platform></Platform>
+			<Screens></Screens>
+			<Benefits></Benefits>
+			<Quote></Quote>
+			<Portfolio></Portfolio>
+			<Footer></Footer>
+		</div>
+	)
+}
+
+export default App
