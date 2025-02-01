43ce1e3be469c57213b88b25c77ce32e15dc00d4
ND <aspizhav@gmail.com>
2025-02-01 19:05:23 +0100

    fix

--- src/components/Home/Quote/Quote.jsx
+++ src/components/Home/Quote/Quote.jsx
@@ -14,6 +14,7 @@ export function Quote() {
 			author: 'Jen Kyung ',
 			position: 'CEO, NextGen Underwriting (Fmr) Chief Underwriter, USAA',
 			picture: JenKyung,
+			link: 'https://www.linkedin.com/in/jennifer-hoolehan-kyung/',
 		},
 		{
 			id: 2,
@@ -22,6 +23,7 @@ export function Quote() {
 			position:
 				'Head of Strategic Planning & Execution AmTrust Financial Services',
 			picture: Vijay,
+			link: 'https://www.linkedin.com/in/vijaylaknidhi/',
 		},
 		{
 			id: 3,
@@ -29,35 +31,52 @@ export function Quote() {
 			author: 'Rob Brewer',
 			position: '(Fmr) CUO, Middle Market The Hanover Insurance Group',
 			picture: RobBrewer,
+			link: 'https://www.linkedin.com/in/rob-brewer-93a12015/',
 		},
 	]
 
 	const containerRef = useRef(null)
+	const indexRef = useRef(0) // зберігає поточний індекс без ререндеру
 	const [index, setIndex] = useState(0)
-	const [direction, setDirection] = useState(1)
 
+	// Автоматичний скрол
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setIndex(prevIndex => {
-				let newIndex = prevIndex + direction
-				if (newIndex >= quotes.length - 1 || newIndex <= 0) {
-					setDirection(-direction)
-				}
-				return newIndex
-			})
+			let newIndex = indexRef.current + 1
+			if (newIndex >= quotes.length) {
+				newIndex = 0
+			}
+			indexRef.current = newIndex
+			setIndex(newIndex)
+			scrollToIndex(newIndex)
 		}, 5000)
-		return () => clearInterval(interval)
-	}, [direction])
 
-	useEffect(() => {
+		return () => clearInterval(interval)
+	}, [quotes.length])
+
+	// Функція для прокрутки до конкретного індексу
+	const scrollToIndex = newIndex => {
 		if (containerRef.current) {
 			const scrollWidth = containerRef.current.scrollWidth / quotes.length
 			containerRef.current.scrollTo({
-				left: index * scrollWidth,
+				left: newIndex * scrollWidth,
 				behavior: 'smooth',
 			})
 		}
-	}, [index])
+	}
+
+	// Відстеження ручного скролу
+	const handleScroll = () => {
+		if (containerRef.current) {
+			const scrollLeft = containerRef.current.scrollLeft
+			const scrollWidth = containerRef.current.scrollWidth / quotes.length
+			const newIndex = Math.round(scrollLeft / scrollWidth)
+			if (newIndex !== indexRef.current) {
+				indexRef.current = newIndex
+				setIndex(newIndex)
+			}
+		}
+	}
 
 	return (
 		<div className='quotes-scroll'>
@@ -66,10 +85,12 @@ export function Quote() {
 					<div
 						className='securQuote-wrapcont'
 						ref={containerRef}
+						onScroll={handleScroll}
 						style={{
 							display: 'flex',
-							overflow: 'hidden',
+							overflowX: 'auto',
 							scrollBehavior: 'smooth',
+							scrollSnapType: 'x mandatory',
 						}}
 					>
 						<div className='securQuote-star-top'>
@@ -105,9 +126,79 @@ export function Quote() {
 												alt={quote.author}
 												className='securQuotePictureImg'
 											/>
+											<svg
+												onClick={() =>
+													window.open(
+														`{quote.link}`,
+														'_blank',
+														'noopener,noreferrer'
+													)
+												}
+												xmlns='http://www.w3.org/2000/svg'
+												width='24'
+												height='24'
+												viewBox='0 0 24 24'
+												fill='none'
+											>
+												<g clip-path='url(#clip0_3370_29032)'>
+													<path
+														fill-rule='evenodd'
+														clip-rule='evenodd'
+														d='M2.66667 24H21.3333C22.8061 24 24 22.8061 24 21.3333V2.66667C24 1.19391 22.8061 0 21.3333 0H2.66667C1.19391 0 0 1.19391 0 2.66667V21.3333C0 22.8061 1.19391 24 2.66667 24Z'
+														fill='#2E3F5C'
+													/>
+													<path
+														fill-rule='evenodd'
+														clip-rule='evenodd'
+														d='M20.6663 20.6668H17.1049V14.6009C17.1049 12.9377 16.4729 12.0083 15.1566 12.0083C13.7245 12.0083 12.9764 12.9755 12.9764 14.6009V20.6668H9.54412V9.11127H12.9764V10.6678C12.9764 10.6678 14.0084 8.75823 16.4605 8.75823C18.9116 8.75823 20.6663 10.255 20.6663 13.3506V20.6668ZM5.44946 7.59817C4.28036 7.59817 3.33301 6.64338 3.33301 5.46583C3.33301 4.28828 4.28036 3.3335 5.44946 3.3335C6.61855 3.3335 7.56534 4.28828 7.56534 5.46583C7.56534 6.64338 6.61855 7.59817 5.44946 7.59817ZM3.67719 20.6668H7.25614V9.11127H3.67719V20.6668Z'
+														fill='#EBEAEF'
+													/>
+												</g>
+												<defs>
+													<clipPath id='clip0_3370_29032'>
+														<rect width='24' height='24' fill='white' />
+													</clipPath>
+												</defs>
+											</svg>
 										</div>
 										<div className='wrapper-bottom-right'>
-											<div className='wrapper-bottom-name'>{quote.author}</div>
+											<div className='wrapper-bottom-name'>
+												{quote.author}
+												<svg
+													onClick={() =>
+														window.open(
+															`{quote.link}`,
+															'_blank',
+															'noopener,noreferrer'
+														)
+													}
+													xmlns='http://www.w3.org/2000/svg'
+													width='24'
+													height='24'
+													viewBox='0 0 24 24'
+													fill='none'
+												>
+													<g clip-path='url(#clip0_3370_29032)'>
+														<path
+															fill-rule='evenodd'
+															clip-rule='evenodd'
+															d='M2.66667 24H21.3333C22.8061 24 24 22.8061 24 21.3333V2.66667C24 1.19391 22.8061 0 21.3333 0H2.66667C1.19391 0 0 1.19391 0 2.66667V21.3333C0 22.8061 1.19391 24 2.66667 24Z'
+															fill='#2E3F5C'
+														/>
+														<path
+															fill-rule='evenodd'
+															clip-rule='evenodd'
+															d='M20.6663 20.6668H17.1049V14.6009C17.1049 12.9377 16.4729 12.0083 15.1566 12.0083C13.7245 12.0083 12.9764 12.9755 12.9764 14.6009V20.6668H9.54412V9.11127H12.9764V10.6678C12.9764 10.6678 14.0084 8.75823 16.4605 8.75823C18.9116 8.75823 20.6663 10.255 20.6663 13.3506V20.6668ZM5.44946 7.59817C4.28036 7.59817 3.33301 6.64338 3.33301 5.46583C3.33301 4.28828 4.28036 3.3335 5.44946 3.3335C6.61855 3.3335 7.56534 4.28828 7.56534 5.46583C7.56534 6.64338 6.61855 7.59817 5.44946 7.59817ZM3.67719 20.6668H7.25614V9.11127H3.67719V20.6668Z'
+															fill='#EBEAEF'
+														/>
+													</g>
+													<defs>
+														<clipPath id='clip0_3370_29032'>
+															<rect width='24' height='24' fill='white' />
+														</clipPath>
+													</defs>
+												</svg>
+											</div>
 											<div className='wrapper-bottom-position-home'>
 												{quote.position}
 											</div>
